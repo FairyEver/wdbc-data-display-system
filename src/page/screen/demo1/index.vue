f4526ffbc8494059aed85507839ecd8ce8ab56f1
李杨 <1711467488@qq.com>
2018-03-19 14:38:22 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="container">
-    Hello
+    screen1
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'index',
-      component: resolve => { require(['@/components/layout-main/index.vue'], resolve) },
+      component: resolve => { require(['@/components/layout-main'], resolve) },
       children: [
         {
-          path: 'dashboard',
-          name: 'dashboard',
-          component: resolve => { require(['@/page/view/index.vue'], resolve) }
+          path: 'screen/demo1',
+          name: 'screen-demo1',
+          component: resolve => { require(['@/page/screen/demo1'], resolve) }
         }
       ]
     }
