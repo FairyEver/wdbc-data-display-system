613647120891f8495a7274ead42ce02c45f469fc
李杨 <1711467488@qq.com>
2018-03-19 17:02:07 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container flex-group col">
-    
+    <div class="flex-item" style="height: 100px;">
+      header
+    </div>
+    <div class="flex-item grow">
+      body
+    </div>
   </div>
 </template>
