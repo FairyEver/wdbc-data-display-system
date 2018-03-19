51fe3ba6ec8fd478b91aaf52f074e425520ad278
李杨 <1711467488@qq.com>
2018-03-19 16:22:58 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="container">
-    <div class="screen__header">
-      screen__header
-    </div>
-    <div class="screen__body">
-      screen__body
-    </div>
+  <div class="container flex-group col">
+    
   </div>
 </template>
