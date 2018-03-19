90ace2dfc8815472af1bc8ba478ad324d89468b6
李杨 <1711467488@qq.com>
2018-03-19 11:30:25 +0800

    no message

--- src/components/layout-main/index.vue
+++ src/components/layout-main/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="layout-main">
-    <div class="layout-main__header">Hello</div>
+    <div class="layout-main__header">
+      <div class="container">Hello</div>
+    </div>
     <div class="layout-main__main">
       <router-view></router-view>
     </div>
