a217540f788fa94544f94f9281eda02492e3d0c4
李杨 <1711467488@qq.com>
2018-03-19 11:38:05 +0800

    no message

--- src/components/layout-main/index.vue
+++ src/components/layout-main/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="layout-main">
-    <div class="layout-main__header">
-      <div class="container">Hello</div>
-    </div>
     <div class="layout-main__main">
       <router-view></router-view>
+    </div>
+    <div class="layout-main__dashboard">
+      测试控制台
     </div>
   </div>
 </template>
