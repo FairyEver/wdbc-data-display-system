20dbf03314ec27746f65784a08a086832a6df2a4
李杨 <1711467488@qq.com>
2018-03-20 10:28:13 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="container flex-group col">
-    <div class="flex-item hov" style="height: 100px;">
-      header
+    <div class="flex-item flex-group col center screen-header" style="height: 100px;">
+      <p class="title">页面标题</p>
+      <p class="sub-title">页面小标题</p>
     </div>
-    <div class="flex-item grow flex-group row">
+    <div class="flex-item grow flex-group row" style="padding: 5px;">
       <!-- L -->
       <div class="flex-item grow flex-group col">
-        <div class="flex-item grow hov"></div>
-        <div class="flex-item grow hov"></div>
+        <div class="flex-item grow hov" style="margin: 5px;">
+          L1
+        </div>
+        <div class="flex-item grow hov" style="margin: 5px;">
+          L2
+        </div>
       </div>
       <!-- R -->
       <div class="flex-item grow flex-group col">
-        <div class="flex-item grow hov"></div>
-        <div class="flex-item grow hov"></div>
+        <div class="flex-item grow hov" style="margin: 5px;">
+          R1
+        </div>
+        <div class="flex-item grow hov" style="margin: 5px;">
+          R2
+        </div>
       </div>
     </div>
   </div>
