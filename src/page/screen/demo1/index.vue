bb8b0fe16be17c048439398147c382b7c702133f
李杨 <1711467488@qq.com>
2018-03-20 09:59:19 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="container flex-group col">
-    <div class="flex-item" style="height: 100px;">
+    <div class="flex-item hov" style="height: 100px;">
       header
     </div>
-    <div class="flex-item grow">
-      body
+    <div class="flex-item grow flex-group row">
+      <!-- L -->
+      <div class="flex-item grow flex-group col">
+        <div class="flex-item grow hov"></div>
+        <div class="flex-item grow hov"></div>
+      </div>
+      <!-- R -->
+      <div class="flex-item grow flex-group col">
+        <div class="flex-item grow hov"></div>
+        <div class="flex-item grow hov"></div>
+      </div>
     </div>
   </div>
 </template>
