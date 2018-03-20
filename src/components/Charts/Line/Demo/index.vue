8fb27fc54eaafdb14cd0a6bbf93685a8353a1d0a
李杨 <1711467488@qq.com>
2018-03-20 15:05:01 +0800

    no message

--- src/components/Charts/Line/Demo/index.vue
+++ src/components/Charts/Line/Demo/index.vue
@@ -20,6 +20,49 @@ export default {
         height: `${this.height}px`,
         width: `${this.width}px`
       }
+    },
+    option () {
+      return {
+        title: {
+          text: 'ECharts 入门示例',
+          left: 'center',
+          textStyle: {
+            color: '#FFF'
+          }
+        },
+        grid: {
+          left: '6%',
+          right: '6%',
+          bottom: 30
+        },
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+          axisLine: {
+            lineStyle: {
+              color: '#FFF'
+            }
+          }
+        },
+        yAxis: {
+          axisLine: {
+            lineStyle: {
+              color: '#FFF'
+            }
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#FFF'
+            }
+          }
+        },
+        series: [
+          {
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
     }
   },
   mounted () {
@@ -32,47 +75,7 @@ export default {
           // 基于准备好的dom，初始化echarts实例
           var myChart = this.echarts.init(this.$refs.chart)
           // 绘制图表
-          myChart.setOption({
-            title: {
-              text: 'ECharts 入门示例',
-              left: 'center',
-              textStyle: {
-                color: '#FFF'
-              }
-            },
-            grid: {
-              left: '6%',
-              right: '6%',
-              bottom: 30
-            },
-            xAxis: {
-              data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
-              axisLine: {
-                lineStyle: {
-                  color: '#FFF'
-                }
-              }
-            },
-            yAxis: {
-              axisLine: {
-                lineStyle: {
-                  color: '#FFF'
-                }
-              },
-              splitLine: {
-                lineStyle: {
-                  color: '#FFF'
-                }
-              }
-            },
-            series: [
-              {
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-              }
-            ]
-          })
+          myChart.setOption(this.option)
         })
     }
   }
