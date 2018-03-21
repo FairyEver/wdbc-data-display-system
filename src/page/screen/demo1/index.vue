5a9573dcd6bfdb4a184a95a9ac787a4393fcc158
李杨 <1711467488@qq.com>
2018-03-21 14:32:44 +0800

    no message

--- src/page/screen/demo1/index.vue
+++ src/page/screen/demo1/index.vue
@@ -111,49 +111,10 @@
 </template>
 
 <script>
+import mixin from '../mixin'
 export default {
-  data () {
-    return {
-      boxs: [],
-      mountedChartNum: 0,
-      needMountedChartNum: 0
-    }
-  },
-  mounted () {
-    // 注册有多少个图表容器
-    for (const name in this.$refs) {
-      if (/^box-[a-zA-Z0-9-]+-g$/.test(name)) {
-        this.needMountedChartNum++
-      }
-    }
-  },
-  watch: {
-    // 监视已经完成初始化的图表数量
-    mountedChartNum (num) {
-      if (num === this.needMountedChartNum) {
-        this.init()
-      }
-    }
-  },
-  methods: {
-    // 初始化
-    init () {
-      for (const name in this.$refs) {
-        if (/^box-[a-zA-Z0-9-]+-g$/.test(name)) {
-          const chart = this.$refs[`${name}-c`]
-          if (!chart) {
-            return
-          }
-          if (!chart.init) {
-            return
-          }
-          chart.init({
-            height: this.$refs[name].offsetHeight,
-            width: this.$refs[name].offsetWidth
-          })
-        }
-      }
-    }
-  }
+  mixins: [
+    mixin
+  ]
 }
 </script>
