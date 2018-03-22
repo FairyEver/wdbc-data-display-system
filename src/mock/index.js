1accef43d9f8c532a0ec3f9d22a4c211e679594c
李杨 <1711467488@qq.com>
2018-03-22 10:43:16 +0800

    no message

--- src/mock/index.js
+++ src/mock/index.js
@@ -21,15 +21,17 @@ Mock.mock(/\.mock/, ({url, type, body}) => {
     })
   } else if (bodyObj.type === 3) {
     return Mock.mock({
-      list: [
-        {name: 'A3', value: '@natural(10, 300)'},
-        {name: 'B3', value: '@natural(10, 300)'},
-        {name: 'C3', value: '@natural(10, 300)'},
-        {name: 'D3', value: '@natural(10, 300)'},
-        {name: 'E3', value: '@natural(10, 300)'},
-        {name: 'F3', value: '@natural(10, 300)'},
-        {name: 'G3', value: '@natural(10, 300)'},
-        {name: 'H3', value: '@natural(10, 300)'}
+      key: 'type1',
+      label: 'TYPE1',
+      data: [
+        {name: 'A', value: '@natural(10, 300)'},
+        {name: 'B', value: '@natural(10, 300)'},
+        {name: 'C', value: '@natural(10, 300)'},
+        {name: 'D', value: '@natural(10, 300)'},
+        {name: 'E', value: '@natural(10, 300)'},
+        {name: 'F', value: '@natural(10, 300)'},
+        {name: 'G', value: '@natural(10, 300)'},
+        {name: 'H', value: '@natural(10, 300)'}
       ]
     })
   }
