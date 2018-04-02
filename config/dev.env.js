ea8416a6c052f0efd7cbca8e91fec7bf58380463
李杨 <1711467488@qq.com>
2018-04-02 14:52:08 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  HOST: "http://192.168.164.120:6080"
 })
