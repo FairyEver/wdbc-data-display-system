58153b2847bc757bbc52d5533c7f78b66fd72378
李杨 <1711467488@qq.com>
2018-04-03 09:18:57 +0800

    no message

--- src/page/screen/page1/index.vue
+++ src/page/screen/page1/index.vue
@@ -5,15 +5,50 @@
     </div>
     <!-- row -->
     <div class="flex-item grow flex-group row" style="padding: 5px; margin-top: -10px;">
-      <!-- col -->
+      <!-- 左侧 -->
+      <div class="flex-item flex-group col" style="width: 30%;">
+        <!-- 三大指数 -->
+        <div class="flex-item" style="text-align: center; line-height: 30px; font-size: 16px;">
+          今日全国行情指数
+        </div>
+        <div class="flex-item flex-group row" style="height: 200px; padding: 5px;">
+          <div ref="box-count-style2-1-g" class="flex-item grow hov" style="margin: 5px;">
+            <ChartCountStyle2
+              ref="box-count-style2-1-g-c"
+              title-text="蛋价指数"
+              :url="`${$root.host}/api/marketQuotationHomeIndex`"
+              :ajax-data="{name: 'djzs'}"
+              :transform="(data) => data.dataInfo.quotationData"
+              @mounted="mountedChartNum++">
+            </ChartCountStyle2>
+          </div>
+          <div ref="box-count-style2-2-g" class="flex-item grow hov" style="margin: 5px;">
+            <ChartCountStyle2
+              ref="box-count-style2-2-g-c"
+              title-text="成本指数"
+              :url="`${$root.host}/api/marketQuotationHomeIndex`"
+              :ajax-data="{name: 'cbzs'}"
+              :transform="(data) => data.dataInfo.quotationData"
+              @mounted="mountedChartNum++">
+            </ChartCountStyle2>
+          </div>
+          <div ref="box-count-style2-3-g" class="flex-item grow hov" style="margin: 5px;">
+            <ChartCountStyle2
+              ref="box-count-style2-3-g-c"
+              title-text="盈利指数"
+              :url="`${$root.host}/api/marketQuotationHomeIndex`"
+              :ajax-data="{name: 'ylzs'}"
+              :transform="(data) => data.dataInfo.quotationData"
+              @mounted="mountedChartNum++">
+            </ChartCountStyle2>
+          </div>
+        </div>
+      </div>
+      <!-- 中间 -->
       <div class="flex-item grow flex-group col">
         
       </div>
-      <!-- col -->
-      <div class="flex-item grow flex-group col">
-        
-      </div>
-      <!-- col -->
+      <!-- 右侧 -->
       <div class="flex-item grow flex-group col">
         
       </div>
