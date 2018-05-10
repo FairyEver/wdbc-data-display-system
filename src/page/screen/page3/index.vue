7f0a664c5865f690d73e6974638c02e3b166c832
liyang <1711467488@qq.com>
2018-05-10 15:58:50 +0800

    no message

--- src/page/screen/page3/index.vue
+++ src/page/screen/page3/index.vue
@@ -6,18 +6,49 @@
     <!-- row -->
     <div class="flex-item flex-group row" style="height: 180px; padding: 5px; margin-top: -10px;">
       <div class="flex-item grow flex-group row">
-        <div class="flex-item grow hov" style="margin: 5px;">1</div>
-        <div class="flex-item grow hov" style="margin: 5px;">2</div>
-        <div class="flex-item grow hov" style="margin: 5px;">3</div>
-        <div class="flex-item grow hov" style="margin: 5px;">4</div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">年产蛋规模</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">纯系</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">祖代</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">父母代</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
       </div>
       <div class="flex-item grow flex-group row">
-        <div class="flex-item grow hov" style="margin: 5px;">1</div>
-        <div class="flex-item grow hov" style="margin: 5px;">2</div>
-        <div class="flex-item grow hov" style="margin: 5px;">3</div>
-        <div class="flex-item grow hov" style="margin: 5px;">4</div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">年孵化能力</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">孵化基地</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">孵化厅</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
+        <div class="flex-item grow flex-group col hov" style="margin: 5px;">
+          <div class="flex-item cell-title">单厅孵化能力</div>
+          <div class="flex-item grow">394</div>
+          <div class="flex-item cell-unit">万套</div>
+        </div>
       </div>
-      
     </div>
     <!-- row -->
     <div class="flex-item grow flex-group row" style="padding: 5px; margin-top: -10px;">
@@ -55,3 +86,22 @@ export default {
   ]
 }
 </script>
+
+<style lang="scss">
+.cell-title {
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  font-size: 30px;
+  color: '#FFF';
+  font-weight: bold;
+}
+.cell-unit {
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  font-size: 16px;
+  color: '#FFF';
+}
+</style>
+
