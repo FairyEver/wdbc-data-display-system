5440e21beeb4665ac26ecaddba83b1b730797c91
孙昊翔 <673686754@qq.com>
2018-07-18 16:50:14 +0800

    no message

---
+++ src/page/screen/page5/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container flex-group col abs-full">
+    <div class="flex-item flex-group col center screen-header" style="height: 80px;">
+      <p class="title">智慧蛋鸡大数据平台 坐堂兽医</p>
+    </div>
+  </div>
+</template>
