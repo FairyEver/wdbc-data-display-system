7e323a45deeaca68efb5e8e8d16f7f074881808d
孙昊翔 <673686754@qq.com>
2018-07-18 19:05:48 +0800

    no message

---
+++ src/components/Charts/Bar/Rank/index.vue
@@ -0,0 +1,168 @@
+<template>
+  <div ref="chart" :style="style"></div>
+</template>
+
+<script>
+import mixin from '@/components/Charts/mixin.js'
+export default {
+  mixins: [
+    mixin
+  ],
+  props: {
+    // 标题
+    titleText: {type: String, required: false, default: 'Chart'},
+    titleColor: {type: String, required: false, default: '#FFF'},
+    titleSize: {type: String, required: false, default: '18'},
+    // // 接口地址
+    // url: {type: String, required: false, default: 'x.mock'},
+    // // 发送请求的时候带的参数
+    // ajaxData: {type: Object, required: false, default: () => ({})},
+    // // 发送请求的间隔
+    // interval: {type: Number, required: false, default: 10000},
+    // grid 设置
+    gridTop: {type: String, required: false, default: '40'},
+    gridBottom: {type: String, required: false, default: '20'},
+    gridLeft: {type: String, required: false, default: '20'},
+    gridRight: {type: String, required: false, default: '5%'}
+  },
+  data () {
+    return {
+      // 宽高
+      height: 0,
+      width: 0,
+      // 定时器
+      intervalObj: null
+    }
+  },
+  computed: {
+    // 容器样式
+    style () {
+      return {
+        height: `${this.height}px`,
+        width: `${this.width}px`
+      }
+    },
+    option () {
+      return {
+        title: {
+          text: this.titleText,
+          top: '10',
+          left: 'center',
+          textStyle: {
+            color: this.titleColor,
+            fontSize: this.titleSize
+          }
+        },
+        grid: {
+          left: this.gridLeft,
+          right: this.gridRight,
+          top: this.gridTop,
+          bottom: this.gridBottom,
+          containLabel: true
+        },
+        yAxis: {
+          data: ['巴西','印尼','美国','印度','中国','世界人口(万)'],
+          axisLine: {
+            // 坐标轴
+            lineStyle: {
+              color: '#434B56'
+            }
+          },
+          axisTick: {
+            // 刻度
+            show: false
+          },
+          axisLabel: {
+            // 坐标文字
+            fontSize: 10,
+            color: '#BCC4CE'
+          }
+        },
+        xAxis: {
+          position: 'top',
+          axisLine: {
+            // 坐标轴
+            lineStyle: {
+              color: '#434B56'
+            }
+          },
+          splitLine: {
+            // 网格区域分割线
+            lineStyle: {
+              color: '#333945'
+            }
+          },
+          axisTick: {
+            // 刻度
+            show: false
+          },
+          axisLabel: {
+            // 坐标文字
+            fontSize: 10,
+            color: '#BCC4CE'
+          }
+        },
+        series: [
+          {
+            type: 'bar',
+            data: [
+              {
+                name: '2012年',
+                type: 'bar',
+                data: [19325, 23438, 31000, 121594, 134141, 681807]
+              }
+            ],
+            barMaxWidth: 14,
+            itemStyle: {
+              normal: {
+                color: '#DD9C38',
+                barBorderRadius: [0, 1, 1, 0]
+              }
+            }
+          }
+        ]
+      }
+    }
+  },
+  // mounted () {
+  //   this.$emit('mounted')
+  // },
+  // methods: {
+  //   // 请求数据 这个函数最后应该返回接口的数据
+  //   getData () {
+  //     return new Promise(async (resolve, reject) => {
+  //       const res = await this.$http.post(this.url, {
+  //         type: 2,
+  //         ...this.ajaxData
+  //       })
+  //       resolve(res.data)
+  //     })
+  //   },
+  //   // 返回拼好的option
+  //   optionMaker () {
+  //     return new Promise(async (resolve, reject) => {
+  //       const data = this.transform(await this.getData())
+  //       const option = this.option
+  //       option.yAxis.data = data.map(e => e.name)
+  //       option.series[0].data = data.map(e => e.value)
+  //       resolve(option)
+  //     })
+  //   },
+  //   // 初始化
+  //   init ({height, width}) {
+  //     this.updateSize(height, width)
+  //       .then(async () => {
+  //         this.chart = this.echarts.init(this.$refs.chart)
+  //         this.chart.setOption(await this.optionMaker())
+  //         if (this.interval) {
+  //           this.intervalObj = setInterval(this.refresh, this.interval)
+  //         }
+  //       })
+  //   },
+  //   // 更新数据
+  //   async refresh () {
+  //     this.chart.setOption(await this.optionMaker())
+  //   }
+  // }
+}
+</script>
--- src/page/screen/page5/index.vue
+++ src/page/screen/page5/index.vue
@@ -42,14 +42,14 @@
       <div class="flex-item grow flex-group col">
         <!-- 地图 -->
         <div ref="box-map-center-g" class="flex-item grow hov" style="margin: 5px;">
-          <ChartMapStyle2
+          <ChartMapStyle5
             ref="box-map-center-g-c"
             title-text="问题解答次数全国分布"
             :url="`${$root.host}/api/getCountryFarmDistributing`"
             :transform="(data) => data.dataInfo.result"
             @initDone="handleMapInitDone"
             @mounted="mountedChartNum++">
-          </ChartMapStyle2>
+          </ChartMapStyle5>
         </div>
         <!-- 三种蛋鸡分布 -->
         <div class="flex-item hov flex-group row" style="height: 160px; margin: 5px; padding: 20px 0px;">
@@ -92,58 +92,8 @@
       <!-- 右侧 -->
       <div class="flex-item flex-group col" style="width: 25%;">
         <!-- 全国养殖户户数TOP10 -->
-        <div class="flex-item grow flex-group row hov" style="margin: 5px;">
-          <div class="flex-item flex-group col" style="width: 100px;">
-            <!-- 三块 -->
-            <div class="flex-item grow flex-group col">
-              <div ref="box-pie-style3-r1-g" class="flex-item grow">
-                <ChartPieStyle3
-                  ref="box-pie-style3-r1-g-c"
-                  :radius="['60%', '70%']"
-                  :data="distributingRightRed"
-                  :color-light="pieColors[0]"
-                  @mounted="mountedChartNum++">
-                </ChartPieStyle3>
-              </div>
-              <div class="flex-item center-pie-title mini" :style="{color: pieColors[0]}">红壳占比</div>
-            </div>
-            <div class="flex-item grow flex-group col">
-              <div ref="box-pie-style3-r2-g" class="flex-item grow">
-                <ChartPieStyle3
-                  ref="box-pie-style3-r2-g-c"
-                  :radius="['60%', '70%']"
-                  :data="distributingRightPink"
-                  :color-light="pieColors[1]"
-                  @mounted="mountedChartNum++">
-                </ChartPieStyle3>
-              </div>
-              <div class="flex-item center-pie-title mini" :style="{color: pieColors[1]}">粉壳占比</div>
-            </div>
-            <div class="flex-item grow flex-group col">
-              <div ref="box-pie-style3-r3-g" class="flex-item grow">
-                <ChartPieStyle3
-                  ref="box-pie-style3-r3-g-c"
-                  :radius="['60%', '70%']"
-                  :data="distributingRightWhite"
-                  :color-light="pieColors[2]"
-                  @mounted="mountedChartNum++">
-                </ChartPieStyle3>
-              </div>
-              <div class="flex-item center-pie-title mini" :style="{color: pieColors[2]}">白壳占比</div>
-            </div>
-          </div>
-          <div ref="box-map-mini-g" class="flex-item grow">
-            <ChartMapMini
-              ref="box-map-mini-g-c"
-              :title-text="`${mapMiniType}养殖户分布`"
-              :map-type="mapMiniType"
-              :map-data="mapMiniData"
-              @mounted="mountedChartNum++">
-            </ChartMapMini>
-          </div>
-        </div>
         <!-- 全国养殖户年龄分布 -->
-        <div ref="box-bar-style2-2-g" class="flex-item grow hov" style="margin: 5px;">
+        <!-- <div ref="box-bar-style2-2-g" class="flex-item grow hov" style="margin: 5px;">
           <ChartBarStyle2
             ref="box-bar-style2-2-g-c"
             :title-text="`${mapMiniType}养殖户年龄分布`"
@@ -154,9 +104,26 @@
             :interval="0"
             @mounted="mountedChartNum++">
           </ChartBarStyle2>
-        </div>
+        </div> -->
         <!-- 全国养殖户从业年限分布 -->
-        <div ref="box-pie-style2-2-g" class="flex-item grow hov" style="margin: 5px;">
+        <div ref="box-bar-top-1-g" class="flex-item grow hov" style="margin: 5px;">
+          <ChartBarTop10
+            ref="box-bar-top-1-g-c"
+            :url="`${$root.host}/api/getFarmTop10`"
+            :transform="(data) => data.dataInfo.result"
+            title-text="全国养殖户户数TOP10"
+            @mounted="mountedChartNum++">
+          </ChartBarTop10>
+        </div>
+        <div class="flex-item grow hov" style="margin: 5px;">
+          <ChartBarTop10
+            :url="`${$root.host}/api/getFarmTop10`"
+            :transform="(data) => data.dataInfo.result"
+            title-text="全国养殖户户数TOP10"
+            @mounted="mountedChartNum++">
+          </ChartBarTop10>
+        </div>
+        <!-- <div ref="box-pie-style2-2-g" class="flex-item grow hov" style="margin: 5px;">
           <ChartPieStyle2
             ref="box-pie-style2-2-g-c"
             :title-text="`${mapMiniType}养殖户从业年限分布`"
@@ -167,7 +134,7 @@
             :interval="0"
             @mounted="mountedChartNum++">
           </ChartPieStyle2>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
