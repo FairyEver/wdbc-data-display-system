35fcf38bad55cf7302cf0a9fea7b97561f896b87
liyang <1711467488@qq.com>
2018-11-15 14:35:21 +0800

    no message

--- src/page/screen/page8/index.vue
+++ src/page/screen/page8/index.vue
@@ -4,7 +4,9 @@
       src="http://210.12.220.210:8099/UpLoad/ykshow/Apps/Sandcastle/pages/5/index.html"
       frameborder="0"
       height="1080"
-      width="1920">
+      width="1920"
+      scrolling="no"
+      style="display: block;">
     </iframe>
   </div>
 </template>
